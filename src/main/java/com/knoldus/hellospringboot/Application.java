99d920da42ac0f1b6549a0a3e611fa389ff31838
robin <robin_0695@hotmail.com>
2022-06-19 12:23:50 +0000

    update 134

--- src/main/java/com/knoldus/hellospringboot/Application.java
+++ src/main/java/com/knoldus/hellospringboot/Application.java
@@ -21,7 +21,7 @@ public class Application {
 
     @Bean
     RouterFunction<ServerResponse> routes() {
-        return route(GET("/api/hello"), request -> ok().syncBody("Hello, SpringBootKnolxDemoTest"));
+        return route(GET("/api/hello"), request -> ok().syncBody("Hello, SpringBootDemoTest"));
     }
 
     public static void main(String[] args) {
